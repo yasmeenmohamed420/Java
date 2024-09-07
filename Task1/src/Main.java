7e74398a5d65587020791284aecb1f1c42b56d45
yasmeenmohamed420 <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-09-08 01:42:34 +0300

    Task solution

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String[]weekDay ={"Saturday", "Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        System.out.println("Please enter number from 0 to 6");
+
+        while(true)
+        {
+            Scanner scan = new Scanner(System.in);
+            int number = scan.nextInt();
+            for (int i = 0 ; i<weekDay.length ;i++)
+            {
+                if (number >= 0 && number <= 6)
+                {
+                    System.out.println(weekDay[number]);
+                    break;
+                }
+                else
+                {
+                    System.out.println("Out of range, Enter anther number between 0 to 6");
+                    break;
+                }
+            }
+
+            System.out.println("Enter another number: ");
+        }
     }
 }
