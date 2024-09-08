2b0c81de53243b0fa9a78c3cbcbe47d02b4ef963
yasmeenmohamed420 <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-09-08 05:37:49 +0300

    task3

--- Task_3/src/Main.java
+++ Task_3/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int count=1;
+        System.out.println("Enter name : ");
+        Scanner sacne = new Scanner(System.in);
+        String name = sacne.next();
+        char[] chars = name.toCharArray();
+
+        for(int i=0 ; i<name.length();i++)
+        {
+            for(int j=i+1; j<name.length();j++)
+            {
+                if(chars[i]==chars[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
     }
 }
