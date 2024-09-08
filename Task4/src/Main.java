7236b8581e7b4759b84aeb69347814731fde3122
yasmeenmohamed420 <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-09-08 04:07:09 +0300

    done task4

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println( "please enter numbers : ");
+        Scanner scane = new Scanner(System.in);
+
+        while (true)
+        {
+            int num1 = scane.nextInt();
+            int num2 = scane.nextInt();
+            int num3 = scane.nextInt();
+            int num4 = scane.nextInt();
+
+            if ((num1 == num2) && (num2 == num3) && (num3 == num4))
+            {
+                System.out.println( "All numbers are equal");
+            }
+            else if ((num1 != num2) && (num1 != num3) && (num1 != num4) && (num2 != num3) && (num2 != num4) && (num3 1!= num4))
+            {
+                System.out.println("All numbers are different");
+            }
+            else
+            {
+                System.out.println("Neither all are equal nor different");
+            }
+            System.out.println("Enter another number: ");
+        }
     }
 }
