2fcd1bbce6832491b71e3c3a325414d3e34810bf
yasmeenmohamed420 <67623624+yasmeenmohamed420@users.noreply.github.com>
2024-09-09 01:18:24 +0300

    Task1

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String[] Names= {"apple", "bat", "car", "dog", "elephant"};
+
+        for (int i = 0; i < Names.length; i++)
+        {
+            for (int j = i + 1; j < Names.length; j++)
+            {
+                if (Names[j].length() < Names[i].length() || (Names[j].length() == Names[i].length() && Names[j].compareTo(Names[i]) < 0)) {
+                    String temp = Names[i];
+                    Names[i] = Names[j];
+                    Names[j] = temp;
+                }
+            }
+        }
+
+        for (int l = 0; l<Names.length;l++)
+        {
+            System.out.println(Names[l]);
+        }
     }
 }
